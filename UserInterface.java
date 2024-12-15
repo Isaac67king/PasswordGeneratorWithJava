806ae1f6940434d6d2132d97475fb1bd025dc146
Isaac (Yis) <162574751+Isaac67king@users.noreply.github.com>
2024-12-15 15:56:16 +0400

    Create UserInterface.java

---
+++ UserInterface.java
@@ -0,0 +1,8 @@
+public interface UserInterface {
+    public int getPassordLength();
+    public boolean addUpperCase();
+    public boolean addLowerCase();
+    public boolean addNumber();
+    public boolean addSymbols();
+    public void displayPassord(String passord);
+}
